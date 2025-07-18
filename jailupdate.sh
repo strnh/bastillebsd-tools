0b631988b0f17c0c8f6419a05b5d569a8a850d6c
strnh <Sotaro.Hikosaka@gmail.com>
2025-07-18 16:03:14 +0900

    Update jailupdate.sh
    for RELEASE 1.0

--- jailupdate.sh
+++ jailupdate.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
-# bastille upgraded: 0.10.20231125_1 -> 0.13.20250126 
-# 
+# bastille upgraded: 0.10.20231125_1 -> 0.13.20250126
+# bastille RELEASE 1.0.x : -> RELS
+#
 VERSION=$(bastille version | awk -F"." '{print $1"."$2}' )
 OLD=0.10
+NEW=0.13
+NEW2=0.14
+RELS=1.0
 
-if [ "`echo \"if (${VERSION} > ${OLD}) 1\" | bc`" -eq 1 ] ; then
- 
- /usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $5  }'  | /bin/sh -x
+case $VERSION in 
+  $OLD)
+   /usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $1  }'  | /bin/sh -x
+  ;;
+  $NEW)
+  $NEW2)
+  
+  /usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $5  }'  | /bin/sh -x
+  ;;
 
-else
+  $RELS) 
 
-/usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $1  }'  | /bin/sh -x
+   /usr/local/bin/bastille list | awk  '{ if ($1!="JID") print "bastille update " $2  }'  | /bin/sh -x
+
+esac
 
 fi
+
