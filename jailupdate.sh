03091acb5b6b8ba9069159b893646ba4d86f81c2
strnh <Sotaro.Hikosaka@gmail.com>
2026-02-24 11:53:40 +0900

    Rewrite jailupdate.sh for simplified updates
    Rewrote the jail update script to simplify the update process using freebsd-update.
    
    変更理由:  bastille update の処理が、OS付属のアップデートスクリプトに従って、非対話的ターミナル経由でのアップデートが全てスルーするようになったことを把握対応。

--- jailupdate.sh
+++ jailupdate.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
-# bastille upgraded: 0.10.20231125_1 -> 0.13.20250126
-# bastille RELEASE 1.0.x : -> RELS
 #
-VERSION=$(bastille version | awk -F"." '{print $1"."$2}' )
-OLD=0.10
-NEW=0.13
-NEW2=0.14
-RELS=1.0
-
-case $VERSION in 
- $OLD)
-   /usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $1  }'  | /bin/sh -x
- ;;
- $NEW)
-    /usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $5  }'  | /bin/sh -x
- ;;
- $NEW2)
-   /usr/local/bin/bastille list -a | awk  '{ if ($1!="JID") print "bastille update " $5  }'  | /bin/sh -x
- ;;
- $RELS) 
-   /usr/local/bin/bastille list | awk  '{ if ($1!="JID") print "bastille update " $2  }'  | /bin/sh -x
- ;;
- esac
-
-
+# 2026/02/24 rewrite.
+#
+set -ue
+#
+/usr/local/bin/bastille list | awk  '{ if ($1!="JID") print "PAGER='cat'  freebsd-update fetch install --not-running-from-cron -j " $1  }'  | /bin/sh -x
